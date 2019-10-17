dc765b5dd50d89aba49293a03d7512767af9cca2
mymany <p675039s@mail.kyutech.jp>
2019-10-18 02:35:57 +0900

    [update] remove typescript packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,25 @@
-parser: '@typescript-eslint/parser'
-plugins: ['@typescript-eslint', 'prettier']
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true
+    },
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:vue/recommended",
+      "plugin:prettier/recommended"
+    ],
+    plugins: [
+      'vue'
+    ],
+    // add your custom rules here
+    rules: {
+      "semi": [2, "never"],
+      "no-console": "off",
+      "vue/max-attributes-per-line": "off",
+      "prettier/prettier": ["error", { "semi": false }]
+    }
+  }
