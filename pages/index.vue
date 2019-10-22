130cf8d66f73258eeac52c9be4225475b8ce7236
mymany <p675039s@mail.kyutech.jp>
2019-10-23 00:49:30 +0900

    [update] index for pug fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,17 @@
-<template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
+<template lang="pug">
+  section.section
+    .columns.is-mobile
+      card(title="Free" icon="github-circle") Open source on
+        a(href="https://github.com/buefy/buefy") GitHub
 
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
+      card(title="Responsive" icon="cellphone-link")
+        b.has-text-grey Every component is responsive
 
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+      card(title="Modern" icon="alert-decagram") Built with 
+        a(href="https://vuejs.org/") Vue.js and
+        a(href="http://bulma.io/") Bulma
+      card(title="Lightweight" icon="arrange-bring-to-front") No other internal dependency
 </template>
-
 <script>
 import { Component, Vue } from 'vue-property-decorator'
 import Card from '~/components/Card'
